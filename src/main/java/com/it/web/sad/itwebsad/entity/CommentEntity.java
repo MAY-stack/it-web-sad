0a97c7851b024ef75b117518e49afacabee2b5ec
Marisol <s.min.g7904@gmail.com>
2023-08-23 13:09:42 +0900

    crud(+@NotNull)

--- src/main/java/com/it/web/sad/itwebsad/entity/CommentEntity.java
+++ src/main/java/com/it/web/sad/itwebsad/entity/CommentEntity.java
@@ -1,10 +1,9 @@
 package com.it.web.sad.itwebsad.entity;
 
-import lombok.Data;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
@@ -13,22 +12,29 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "comments")
 public class CommentEntity {
 
+    @NotNull
     private String id;
 
+    @NotNull
     private String storyId;
 
+    @NotNull
     private String type;
 
+    @NotNull
     private String version;
 
+    @NotNull
     private String message;
 
     private String image;
 
     private String isSend;
 
+    @NotNull
     private String time;
 
+    @NotNull
     private UserEntity user;
 
 }
--- src/main/java/com/it/web/sad/itwebsad/entity/UserEntity.java
+++ src/main/java/com/it/web/sad/itwebsad/entity/UserEntity.java
@@ -1,5 +1,6 @@
 package com.it.web.sad.itwebsad.entity;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,10 @@ import lombok.NoArgsConstructor;
 @Data
 public class UserEntity {
 
+    @NotNull
     private String uuid;
 
+    @NotNull
     private String name;
 
     private String ip;
