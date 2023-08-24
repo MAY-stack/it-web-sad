e94ffc8fe0da27619a6e92de773d3fe1a943b315
Marisol <s.min.g7904@gmail.com>
2023-08-24 19:49:14 +0900

    before apprunner deploy test

--- src/main/java/com/it/web/sad/itwebsad/config/MongoConfiguration.java
+++ src/main/java/com/it/web/sad/itwebsad/config/MongoConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.convert.DbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
@@ -11,6 +12,7 @@ import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
+@EnableMongoAuditing
 public class MongoConfiguration {
     @Bean
     public MappingMongoConverter setConvertingOptions(MongoDatabaseFactory factory, MongoMappingContext ctx,
